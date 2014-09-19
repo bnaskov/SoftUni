aa8163d85a38eedd5d44eec381e93e679ed1989c
b.naskov <boni.mislyashki@gmail.com>
2014-09-19 23:30:23 +0300

    Web-Fundamentals homework

---
+++ Web-Fundamentals/05-Semantic-HTML/script/Pr5-JavaScript-Worship.js
@@ -0,0 +1,9 @@
+var jsw = document.getElementById("canvas").getContext("2d");
+jsw.font = "bold 33px serif";
+jsw.textAlign = "center";
+jsw.fillText("I love JavaScript", 135, 50);
+jsw.fillStyle = "#F1DA4E";
+jsw.fillRect(67.5, 80, 135, 135);
+jsw.font = "bold 85px serif";
+jsw.fillStyle = "#000000";
+jsw.fillText("JS", 140, 205);
