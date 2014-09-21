51fc38c664958dbb745e91527f82135f9c2cbaa5
b.naskov <boni.mislyashki@gmail.com>
2014-09-21 12:13:06 +0300

    Exception Handling homework

---
+++ OOP/02-Exception-Handling/01-Square-Root/SquareRoot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Please enter a positive integer: ");
+            long num = Int64.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+            double result = Math.Sqrt(num);
+            Console.WriteLine("The square root of {0} is {1:0.##}.", num, result);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ OOP/02-Exception-Handling/02-Exception-Handling/EnterNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class EnterNumbers
+{
+    static void Main()
+    {
+        int start = 1;
+        int end = 100;
+
+        for (int i = 0; i < 10; i++)
+        {
+            start = ReadNumber(start, end);
+        }
+    }
+
+    private static int ReadNumber(int start, int end)
+    {
+        int num = 0;
+        int tempNum = 0;
+        while (tempNum == num)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number between {0} and {1}: ", start, end);
+                num = int.Parse(Console.ReadLine());
+                while (num <= start || num >= end)
+                {
+                    Console.WriteLine("Invalid input! Your number must be between {0} and {1}!", start, end);
+                    Console.WriteLine("Enter a number between {0} and {1}: ", start, end);
+                    num = int.Parse(Console.ReadLine());
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Number!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number! The number is too big to fit in int32!");
+            }
+        }
+        
+        return num;
+    }
+}
