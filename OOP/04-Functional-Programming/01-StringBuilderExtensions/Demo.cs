541b17da0080d953944414b5ed7c807963ca76cc
b.naskov <boni.mislyashki@gmail.com>
2014-09-29 11:46:49 +0300

    StringBuilder Extensions

---
+++ OOP/04-Functional-Programming/01-StringBuilderExtensions/Demo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder text = new StringBuilder("Write a program to demonstrate that your new extension methods work correctly.");
+        IEnumerable<double> doubles = new List<double>() { 5.3, 2.5, 4.23 };
+
+        string substr = text.Substring(8, 7);
+        Console.WriteLine(substr);
+
+        StringBuilder sb = new StringBuilder();
+        sb = text.RemoveText("write ");
+        Console.WriteLine(sb);
+
+        StringBuilder sbAppend = new StringBuilder();
+        sbAppend = text.AppendAll(doubles);
+        Console.WriteLine(sbAppend);
+    }
+}
---
+++ OOP/04-Functional-Programming/01-StringBuilderExtensions/StringBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class StringBuilderExtensions
+{
+    public static string Substring(this StringBuilder strBuild, int startIndex, int length)
+    {
+        string str = strBuild.ToString();
+        string subStr = str.Substring(startIndex, length);
+        return subStr;
+    }
+
+    public static StringBuilder RemoveText(this StringBuilder str, string text)
+    {
+        var lenght = text.Length;
+        var strAsString = str.ToString();
+
+        while (true)
+        {
+            int textPosition = strAsString.IndexOf(text, StringComparison.CurrentCultureIgnoreCase);
+
+            if (textPosition < 0)
+            {
+                break;
+            }
+
+            strAsString = strAsString.Remove(textPosition, lenght);
+        }
+
+        return str = new StringBuilder(strAsString);
+    }
+
+    public static StringBuilder AppendAll<T>(this StringBuilder strBuild, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            string itemToString = item.ToString();
+            strBuild.Append(itemToString);
+        }
+
+        return strBuild;
+    }
+}
