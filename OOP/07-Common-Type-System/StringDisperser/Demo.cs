9cc5a890ccca1fc6d819e571347f7960a6f807d1
b.naskov <boni.mislyashki@gmail.com>
2014-10-10 10:20:50 +0300

    common type homework

---
+++ OOP/07-Common-Type-System/StringDisperser/Demo.cs
@@ -0,0 +1,25 @@
+﻿namespace StringDisperser
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Demo
+    {
+        static void Main()
+        {
+            StringDisperser sDisp1 = new StringDisperser("Jon", "Snow", "Merry");
+            StringDisperser sDisp2 = new StringDisperser("Jon", "Lanister", "Tyrion");
+            StringDisperser sDisp3 = (StringDisperser)sDisp1.Clone();
+
+            Console.WriteLine(sDisp1);
+
+            Console.WriteLine(sDisp1 == sDisp2);
+            Console.WriteLine(sDisp1 == sDisp3);
+
+            foreach (var ch in sDisp1)
+            {
+                Console.Write(ch + " ");
+            }
+        }
+    }
+}
