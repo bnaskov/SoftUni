200e9836cb6ce525ee0be0ad42b6902a5785b303
b.naskov <boni.mislyashki@gmail.com>
2014-10-20 16:40:33 +0300

    Classes Furniture and Table created

---
+++ OOP/OOP-Exam-Preparation/FurnitureManufacturer/FurnitureManufacturer/Models/Furniture.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FurnitureManufacturer.Interfaces;
+
+namespace FurnitureManufacturer.Models
+{
+    public abstract class Furniture : IFurniture
+    {
+        private string model;
+        private string material;
+        private decimal price;
+        private decimal height;
+
+        public Furniture(string model, string material, decimal price, decimal height)
+        {
+            this.Model = model;
+            this.Material = material;
+            this.Price = price;
+            this.Height = height;
+        }
+        public string Model
+        {
+            get { return this.model; }
+            private set
+            {
+                if (value == null)
+	            {
+		             throw new ArgumentNullException("value", "Furniture model cannot be null.");
+	            }
+
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Model must be at least 3 symbols.");
+                }
+
+                this.model = value;
+            }
+        }
+
+        public string Material
+        {
+            get { return this.model; }
+            private set
+            {
+                this.material = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Furniture price cannot be negative or equal to zeto.");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public decimal Height
+        {
+            get { return this.height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Furniture height cannot be negative or equal to zeto.");
+                }
+
+                this.height = value;
+            }
+        }
+    }
+}
---
+++ OOP/OOP-Exam-Preparation/FurnitureManufacturer/FurnitureManufacturer/Models/Table.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FurnitureManufacturer.Interfaces;
+
+namespace FurnitureManufacturer.Models
+{
+    class Table : Furniture, ITable
+    {
+        private decimal length;
+        private decimal width;
+        private decimal area;
+
+        public Table(string model, string material, decimal price, decimal height, decimal length, decimal width)
+            :base(model, material, price, height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.area = this.Height * this.Width;
+        }
+
+        public decimal Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Table length cannot be negative or equal to zeto.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public decimal Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Table width cannot be negative or equal to zeto.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public decimal Area
+        {
+            get { return this.area; }
+        }
+    }
+}
