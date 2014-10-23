e4cb8845ffa4c01c508860ac641babbb6db392a8
b.naskov <boni.mislyashki@gmail.com>
2014-10-24 00:57:56 +0300

    high quality code documentation homework

---
+++ High-Quality-Code/02-Code-Documentation/CSharp/StringExtensions/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+class ProgramTester
+{
+    static void Main()
+    {
+        Console.WriteLine("19871".ToShort());
+        Console.WriteLine("1258".ToBoolean());
+        Console.WriteLine("2014/10/05".ToDateTime());
+        Console.WriteLine("aJona".GetStringBetween("a", "a"));
+    }
+}
+
