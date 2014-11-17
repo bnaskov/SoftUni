b718dddcc7076cf50021223741e1bd761a849a16
b.naskov <boni.mislyashki@gmail.com>
2014-11-17 15:51:26 +0200

    task 2 solved

---
+++ High-Quality-Code/15-Code-Optimization/Operations-Performance-Tests/Operations-Performance-Tests/Task1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Operations_Performance_Tests
+{
+    class Task1
+    {
+        static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                action();
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        static void Main()
+        {
+            int numInt = 0;
+
+            Console.Write("Int add: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numInt += 1;
+                }
+            });
+
+            Console.Write("Int substract: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numInt -= 1;
+                }
+            });
+
+            Console.Write("Int increment: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numInt++;
+                }
+            });
+
+            Console.Write("Int multiply: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numInt *= 1;
+                }
+            });
+
+            Console.Write("Int divide: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numInt /= 1;
+                }
+            });
+
+            ///
+            long numLong = 0;
+
+            Console.Write("Long add: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numLong += 1;
+                }
+            });
+
+            Console.Write("Long substract: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numLong -= 1;
+                }
+            });
+
+            Console.Write("Long increment: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numLong++;
+                }
+            });
+
+            Console.Write("Long multiply: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numLong *= 1;
+                }
+            });
+
+            Console.Write("Long divide: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numLong /= 1;
+                }
+            });
+
+
+            float numFloat = 0.0f;
+
+            Console.Write("Float add: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numFloat += 1;
+                }
+            });
+
+            Console.Write("Float substract: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numFloat -= 1;
+                }
+            });
+
+            Console.Write("Long increment: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numFloat++;
+                }
+            });
+
+            Console.Write("Float multiply: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numFloat *= 1;
+                }
+            });
+
+            Console.Write("Float divide: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numFloat /= 1;
+                }
+            });
+
+            double numDouble = 0.0;
+
+            Console.Write("Double add: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDouble += 1;
+                }
+            });
+
+            Console.Write("Double substract: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDouble -= 1;
+                }
+            });
+
+            Console.Write("Double increment: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDouble++;
+                }
+            });
+
+            Console.Write("Double multiply: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDouble *= 1;
+                }
+            });
+
+            Console.Write("Double divide: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDouble /= 1;
+                }
+            });
+
+            decimal numDecimal = 0.0M;
+
+            Console.Write("Decimal add: \t\t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDecimal += 1m;
+                }
+            });
+
+            Console.Write("Decimal substract: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDecimal -= 1m;
+                }
+            });
+
+            Console.Write("Decimal increment: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDecimal++;
+                }
+            });
+
+            Console.Write("Decimal multiply: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDecimal *= 1m;
+                }
+            });
+
+            Console.Write("Decimal divide: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    numDecimal /= 1m;
+                }
+            });
+        }
+    }
+}
---
+++ High-Quality-Code/15-Code-Optimization/Operations-Performance-Tests/Task2/Task2.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Task2
+{
+    class Task2
+    {
+        static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                action();
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        static void Main()
+        {
+            float numFloat = 1.0f;
+            double sum = 0;
+
+            Console.Write("Float square: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Sqrt((double)numFloat);
+                }
+            });
+
+            Console.Write("Float log: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Log((double)numFloat);
+                }
+            });
+
+            Console.Write("Float sinus: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Sin((double)numFloat);
+                }
+            });
+
+            double numDouble = 1.0d;
+            sum = 0;
+
+            Console.Write("Double square: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Sqrt(numDouble);
+                }
+            });
+
+            Console.Write("Double log: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Log(numDouble);
+                }
+            });
+
+            Console.Write("Double sinus: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Sin(numDouble);
+                }
+            });
+
+            decimal numDecimal = 1.0M;
+            sum = 0;
+
+            Console.Write("Decimal sqr: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Sqrt((double)numDecimal);
+                }
+            });
+
+            Console.Write("Decimal log: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Log((double)numDecimal);
+                }
+            });
+
+            Console.Write("Decimal sinus: \t");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 1; i <= 100000; i++)
+                {
+                    sum += Math.Sin((double)numDecimal);
+                }
+            });
+        }
+    }
+}
