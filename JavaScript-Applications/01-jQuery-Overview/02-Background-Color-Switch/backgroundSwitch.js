c34b962267729d3927c8df0aaf92c1f79886f8aa
b.naskov <boni.mislyashki@gmail.com>
2014-12-25 09:40:42 +0200

    angular js lesson 2

---
+++ JavaScript-Applications/01-jQuery-Overview/02-Background-Color-Switch/backgroundSwitch.js
@@ -0,0 +1,16 @@
+(function() {
+
+    $(document).ready(function () {
+        $('#paintButton').on('click', changeBackground);
+    });
+
+    function changeBackground(){
+        var $classValue = $('#class').val();
+        var $colorValue = $('#color').val();
+
+        $('.' + $classValue).css('background-color', $colorValue);
+    }
+
+
+
+}());
