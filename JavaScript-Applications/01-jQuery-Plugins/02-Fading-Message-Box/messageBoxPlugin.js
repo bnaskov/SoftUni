eca925dcacae842b5aaa3330c30057ea04f5d4c9
b.naskov <boni.mislyashki@gmail.com>
2014-12-27 12:16:38 +0200

    fading message box added

---
+++ JavaScript-Applications/01-jQuery-Plugins/02-Fading-Message-Box/messageBoxPlugin.js
@@ -0,0 +1,23 @@
+(function($){
+    $.fn.messageBox = function () {
+        var $this;
+        $this = $(this);
+        $this.append($('<h1>').addClass('message'));
+
+        return this;
+    };
+
+    $.fn.success = function (text) {
+        $('.message').html(text).addClass('success').hide().fadeIn(1000).fadeOut(2000);
+
+        return this;
+    };
+
+    $.fn.error = function (text) {
+        $('.message').html(text).addClass('error').hide().fadeIn(1000).fadeOut(2000);
+
+        return this;
+    };
+
+}(jQuery));
+
